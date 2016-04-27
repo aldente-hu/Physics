68ddfb34fc63a0485bae7bfa6bdaa27e1db6e2ba
aldente-hu <aldente@hirosaki-u.ac.jp>
2016-04-27 09:57:31 +0900

    ソースコードを整理。

--- MotionSimulation/RungeKutta.cs
+++ MotionSimulation/RungeKutta.cs
@@ -7,9 +7,10 @@ using System.Threading.Tasks;
 namespace HirosakiUniversity.Aldente.MotionSimulation
 {
 
-
+	#region RungeKuttaクラス
 	public class RungeKutta
 	{
+		// 4次のルンゲクッタ法で，"次"のベクトルを得る関数です．
 		static TimeVector GetNext(TimeVector timeVector, double step, Func<Vector, double, Vector> force)
 		{
 			var vector = timeVector.Vector;
@@ -23,6 +24,17 @@ namespace HirosakiUniversity.Aldente.MotionSimulation
 		}
 
 		// とりあえずstaticで．
+
+		#region *[static]ループ実行(DoLoop)
+		/// <summary>
+		/// 各ステップの，時系列ベクトルデータを返します．
+		/// 各ステップの結果を返します．
+		/// </summary>
+		/// <param name="initialVector">初期ベクトル．</param>
+		/// <param name="force">力を与える関数．(ベクトル,時刻) -> 力です．</param>
+		/// <param name="step">ステップの時間間隔．</param>
+		/// <param name="count">ステップを繰り返す回数．</param>
+		/// <returns></returns>
 		public static IEnumerable<TimeVector> DoLoop
 			(Vector initialVector, Func<Vector, double, Vector> force, double step, int count = 10000)
 		{
@@ -38,9 +50,9 @@ namespace HirosakiUniversity.Aldente.MotionSimulation
 				yield return vector;
 			}
 		}
+		#endregion
 
-
-		// 以下，非static用．
+		#region 以下，非static用．
 
 		public Func<Vector, double, Vector> Force { get; set; }
 		public Double Step { get; set; }
@@ -54,5 +66,9 @@ namespace HirosakiUniversity.Aldente.MotionSimulation
 			}
 		}
 
+		#endregion
+
 	}
+	#endregion
+
 }
