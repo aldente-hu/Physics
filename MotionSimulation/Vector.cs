18c9d7638bd08f00b8bfb868ed6dce781cd60582
aldente-hu <aldente@hirosaki-u.ac.jp>
2016-04-27 10:30:06 +0900

    ソースコードをもうちょっと整理。

--- MotionSimulation/Vector.cs
+++ MotionSimulation/Vector.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace HirosakiUniversity.Aldente.MotionSimulation
 {
+
+	#region Vector構造体
+	/// <summary>
+	/// 2次元ベクトルを表す構造体です．
+	/// 一般的に使えますが，要素名は1次元の位置と速度を意識しています(1次元での位相空間の元)．
+	/// </summary>
 	public struct Vector
 	{
 		public double X;
@@ -38,14 +44,20 @@ namespace HirosakiUniversity.Aldente.MotionSimulation
 			return (1 / a) * u;
 		}
 	}
+	#endregion
 
 	// IEnumerableの代わりにIDictionaryを使うと，yield returnが使えなかった！
 
+	#region TimeVector構造体
+	/// <summary>
+	/// 時刻とベクトルを組み合わせた構造体です．
+	/// </summary>
 	public struct TimeVector
 	{
 		public double Time;
 		public Vector Vector;
 
 	}
+	#endregion
 
 }
